84c33ba35bcbe4017d9f1d69cff95dda9a73df8e
mohdsaws-at-317027708743 <mohd@zapinventory.com>
2025-03-11 13:15:23 +0530

    origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors(
   {
-    origin:["https://tech-short-5kzi-kmsgmsg6x-mohdshahrukh007s-projects.vercel.app/"],
+    origin:["https://vercel.com/mohdshahrukh007s-projects/tech-short-5kzi/"],
     methods:["POST","GET"],
     credentials:true
 
@@ -21,7 +21,7 @@ const setContext = (req, res, next) => {
 };
 app.use(setContext);
 const sampleRoutes = require("../routes/sampleRoutes");
-app.use("https://tech-short-5kzi-kmsgmsg6x-mohdshahrukh007s-projects.vercel.app/api", sampleRoutes);
+app.use("https://vercel.com/mohdshahrukh007s-projects/tech-short-5kzi/api", sampleRoutes);
 // app.use("/api", sampleRoutes);
 
 app.listen(PORT, () => {
