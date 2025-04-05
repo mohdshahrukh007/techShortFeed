63f469cc325b6e6ee8dbaed02651c49d82d4cb3e
mohdsaws-at-317027708743 <mohd@zapinventory.com>
2025-04-05 19:04:51 +0530

    snathy issue fixed

--- frontend/src/app/short.service.ts
+++ frontend/src/app/short.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, map, throwError } from "rxjs";
+import { catchError, map, throwError, of } from "rxjs";
 import { environment } from "src/environment/environment";
+
 @Injectable({
   providedIn: "root",
 })
 export class ShortService {
-  private twitterUrl =""
-  private ytUrl = environment.apiUrl+"/api/shorts?query=";
+  private twitterUrl = "";
+  private ytUrl = environment.apiUrl + "/api/shorts?query=";
 
   constructor(private http: HttpClient) {}
 
