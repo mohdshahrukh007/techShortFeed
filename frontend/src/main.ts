ff13589d7f287b7b2770809834d122f2f30a397e
mohdsaws-at-317027708743 <mohd@zapinventory.com>
2025-04-26 14:22:45 +0530

    Adjust iframe overlay margin in ShortFeedComponent and suppress console logs in main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 
+// Override console.log to suppress logs
+console.log = function () {};
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
